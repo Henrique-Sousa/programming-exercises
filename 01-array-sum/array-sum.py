16eafb1ecc039b5c4d601d5039109cb4305c048c
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-04-21 09:35:11 -0300

    style

--- 01-array-sum/array-sum.py
+++ 01-array-sum/array-sum.py
@@ -1,16 +1,16 @@
 from functools import reduce
 
-def loop_array_sum(arr):
+def imper_array_sum(arr):
     sum = 0
     for el in arr:
         sum += el
     return sum
 
-def array_sum(arr):
+def funct_array_sum(arr):
     return reduce(lambda a, c: a + c, arr)
     
+
 arr = [1, 2, 3, 4, 10, 11]
-res1 = array_sum(arr)
-res2 = loop_array_sum(arr)
-assert res1 == 31
-assert res2 == 31
+
+assert imper_array_sum(arr) == 31
+assert funct_array_sum(arr) == 31
