a9f3d645c8bd342cdf044b52aebcb221ac55e08f
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-04-22 19:32:29 -0300

    constexpr -> const

--- shapes/shapes.h
+++ shapes/shapes.h
@@ -13,13 +13,13 @@ class Quadrilateral : public Shape {
     double side1, side2, side3, side4;
 
   public:
-    constexpr Quadrilateral(double s1, double s2, double s3, double s4)
+    Quadrilateral(double s1, double s2, double s3, double s4)
       : side1(s1), side2(s2), side3(s3), side4(s4) {}
 
-    constexpr double getSide1() const { return side1; }
-    constexpr double getSide2() const { return side2; }
-    constexpr double getSide3() const { return side3; }
-    constexpr double getSide4() const { return side4; }
+    const double getSide1() const { return side1; }
+    const double getSide2() const { return side2; }
+    const double getSide3() const { return side3; }
+    const double getSide4() const { return side4; }
 
     virtual const double area() const = 0;
     const double perimeter() const override { return side1 + side2 + side3 + side4; }
@@ -27,7 +27,7 @@ class Quadrilateral : public Shape {
 
 class Rectangle : public Quadrilateral {
   public:
-    constexpr Rectangle(double length, double width)
+    Rectangle(double length, double width)
       : Quadrilateral(length, width, length, width) {}
     virtual const double area() const override { return side1 * side2; }
     const double perimeter() const override { return 2 * (side1 + side2); }
@@ -35,7 +35,7 @@ class Rectangle : public Quadrilateral {
 
 class Square : public Rectangle { 
   public:
-    constexpr Square(double side) : Rectangle(side, side) {}
+    Square(double side) : Rectangle(side, side) {}
 
     const double area() const override { return side1 * side1; }
     const double perimeter() const override { return 4 * side1; }
@@ -48,7 +48,7 @@ class Circle: public Shape {
   public:
     Circle(double r) : radius(r) {}
 
-    constexpr double getRadius() const { return radius; }
+    const double getRadius() const { return radius; }
     
     const double area() const override { return pi * radius * radius; }
     const double perimeter() const override { return 2 * pi * radius; }
