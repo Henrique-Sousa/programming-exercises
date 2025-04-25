7d60930aac5fa1717e6d593e354cc575005b0ea0
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-04-25 13:37:14 -0300

    add nasm

--- algorithms/01-array-sum/test.sh
+++ algorithms/01-array-sum/test.sh
@@ -3,3 +3,8 @@
 gcc --std=c89 --pedantic c.c && ./a.out && rm ./a.out
 python3 python.py
 node javascript.js
+
+nasm -f elf64 -o nasm64.o nasm64.asm
+ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o nasm64.out -lc nasm64.o
+./nasm64.out
+rm -rf nasm64.o nasm64.out
