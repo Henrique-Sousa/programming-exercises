e7b197eb2c3222191991a3d431fd73f85e9dbae6
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-05-03 21:13:57 -0300

    add native array_sum and array_reduce

--- algorithms/01-array-sum/php.php
+++ algorithms/01-array-sum/php.php
@@ -11,4 +11,6 @@ $arr = array(1, 2, 3, 4, 10, 11);
 $n = 6;
 $sum =  sum_array($arr, $n);
 echo $sum . "\n";
+echo array_sum($arr). "\n";
+echo array_reduce($arr, function($a, $c) { return $a + $c; }) . "\n";
 ?>
